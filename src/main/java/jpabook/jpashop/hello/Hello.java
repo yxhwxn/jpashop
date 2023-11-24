84d3b799f29b064a4f364a0c4e4eea864e3200d5
yxhwxn <officeyh9819@gmail.com>
2023-11-24 18:09:36 +0900

    feat: Add MemberRepository(회원 리포지토리 개발)

---
+++ src/main/java/jpabook/jpashop/hello/Hello.java
@@ -0,0 +1,4 @@
+package jpabook.jpashop.hello;
+
+public class Hello {
+}
--- src/main/java/jpabook/jpashop/hello/HelloController.java
+++ src/main/java/jpabook/jpashop/hello/HelloController.java
@@ -1,4 +1,4 @@
-package jpabook.jpashop;
+package jpabook.jpashop.hello;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
