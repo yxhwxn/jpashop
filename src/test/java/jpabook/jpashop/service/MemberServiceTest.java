d788718b8dddd7f5dcdd502b5696d2f71e4cd268
yxhwxn <officeyh9819@gmail.com>
2023-12-11 23:37:40 +0900

    :white_check_mark: feat: 중복회원 TestCode 추가 & 예외처리

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -11,6 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -36,12 +37,20 @@ public class MemberServiceTest {
         assertEquals(member, memberRepository.findOne(saveId));
     }
 
-    @Test
+    @Test(expected = IllegalStateException.class)
     public void 중복_회원_예외() throws Exception {
         //given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
 
         //when
+        memberService.join(member1);
+        memberService.join(member2); // 여기서 예외가 발생해야 한다 !!
 
         //then
+        fail("예외가 발생해야 한다.");
     }
 }
