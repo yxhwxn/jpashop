0f86dd47b05c0dac927ab65d261561ee90e0ae14
yxhwxn <officeyh9819@gmail.com>
2023-12-14 22:38:10 +0900

    :sparkles: feat: 상품 주문 Test Code Frame추가(상품주문, 주문취소, 상품주문 재고수량초과)

---
+++ src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -0,0 +1,40 @@
+package jpabook.jpashop.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class OrderServiceTest {
+
+    @Test
+    public void 상품주문() throws Exception {
+        //given
+
+        //when
+
+        //then
+    }
+
+    @Test
+    public void 주문취소() throws Exception {
+        //given
+
+        //when
+
+        //then
+    }
+
+    @Test
+    public void 상품주문_재고수량초과() throws Exception {
+        //given
+
+        //when
+
+        //then
+    }
+}
