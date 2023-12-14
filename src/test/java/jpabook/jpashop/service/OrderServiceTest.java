5b873a3e393d23d1b078d47738cf9e82afb2332f
yxhwxn <officeyh9819@gmail.com>
2023-12-14 23:46:09 +0900

    :sparkles: feat: Exception Test code 추가(상문주문_재고수량초과)

--- src/test/java/jpabook/jpashop/service/OrderServiceTest.java
+++ src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -5,6 +5,7 @@ import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.exception.NotEnoughStockException;
 import jpabook.jpashop.repository.OrderRepository;
 import org.junit.Test;
@@ -17,7 +18,12 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.persistence.EntityManager;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
+/**
+ * ❗️본 강의에서는 JPA와의 연동해서 테스트 해보는 것이 목적이라 아래와 같이 통합적인 테스트 코드가 작성되지만,
+ * 실제 서비스에서는 메서드 하나당 단위 테스트가 이루어져야한다❗️
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @Transactional
@@ -33,8 +39,8 @@ public class OrderServiceTest {
     @Test
     public void 상품주문() throws Exception {
         //given
-        Member member = createMember();
-        Book book = createBook();
+        Member member = createMember("회원1", new Address("서울", "경기", "123-123"));
+        Book book = createBook("시골 JPA", 10000, 10);
 
         int orderCount = 2;
 
@@ -54,11 +60,16 @@ public class OrderServiceTest {
     @Test(expected = NotEnoughStockException.class)
     public void 상품주문_재고수량초과() throws Exception {
         //given
+        Member member = createMember("회원1", new Address("서울", "경기", "123-123"));
+        Item item = createBook("시골 JPA", 10000, 10);
 
+        int orderCount = 11;
 
         //when
+        orderService.order(member.getId(), item.getId(), orderCount);
 
         //then
+        fail("재고 수량 부족 예외가 발생해야 한다.");
     }
 
     @Test
@@ -71,19 +82,19 @@ public class OrderServiceTest {
     }
 
     // Dummy data 메서드화
-    private Book createBook() {
+    private Book createBook(String name, int price, int stockQuantity) {
         Book book = new Book();
-        book.setName("시골 JPA");
-        book.setPrice(10000);
-        book.setStockQuantity(10);
+        book.setName(name);
+        book.setPrice(price);
+        book.setStockQuantity(stockQuantity);
         em.persist(book);
         return book;
     }
 
-    private Member createMember() {
+    private Member createMember(String name, Address address) {
         Member member = new Member();
-        member.setName("회원1");
-        member.setAddress(new Address("서울", "경기", "123-123"));
+        member.setName(name);
+        member.setAddress(address);
         em.persist(member);
         return member;
     }
